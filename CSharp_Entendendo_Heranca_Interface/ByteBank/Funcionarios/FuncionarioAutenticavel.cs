67ad07e21d7ddcfb123883403844c0de4ff9bd34
Washington Luiz Santana de Moraes Silva <57022931+WashingtonLuizDev@users.noreply.github.com>
2022-03-21 17:43:51 -0300

    Projeto ByteBank
    Projeto do curso C# parte 3 Entendendo Herança e Interface

---
+++ CSharp_Entendendo_Heranca_Interface/ByteBank/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        public string Senha { get; set; }
+        public FuncionarioAutenticavel(double salario, string cpf)
+            : base(salario, cpf)
+        { 
+        
+        }
+
+        
+    }
+}
