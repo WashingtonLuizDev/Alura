793aadafcec51ece106278fa1a6c5a03034bb298
Washington Luiz Santana de Moraes Silva <57022931+WashingtonLuizDev@users.noreply.github.com>
2022-03-30 16:13:34 -0300

    Commit das Aulas Sobre Exceções
    Primeiro Commit com algumas tarefas das aulas sobre Exceções.

---
+++ CSharp_Entendendo_excecoes/ByteBank/ByteBank/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Profissao { get; set; }
+
+        private string _cpf;
+        
+        public string CPF
+        {
+            get 
+            { 
+                return _cpf; 
+            }
+            set 
+            { 
+                _cpf = value; 
+            }
+        }
+
+    }
+}
---
+++ CSharp_Entendendo_excecoes/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ContaCorrente conta = new ContaCorrente(7480, 874150);
+
+            //Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            try
+            {
+                Metodo();
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine($"{excecao.Message}");
+                Console.WriteLine($"{excecao.StackTrace}");
+                Console.WriteLine($"Aconteceu um erro!");
+            }
+            catch (DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possível fazer uma divisão por 0!");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Exceção com numero = {numero} e divisor = {divisor}");
+                throw;
+            }
+        }
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine($"O Resultado da divisão de 10 por {divisor} é {resultado}");
+        }
+
+    }
+}
